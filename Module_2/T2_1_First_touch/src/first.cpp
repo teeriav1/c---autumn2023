ea800cb34b9dc63a9f1f8c8bbf2e7b2b107ab1ea
teeriav1 <85660952+teeriav1@users.noreply.github.com>
2023-09-16 21:47:45 +0300

    Changed the format into not containing a string on different line

--- Module_2/T2_1_First_touch/src/first.cpp
+++ Module_2/T2_1_First_touch/src/first.cpp
@@ -6,6 +6,6 @@ void Hello() {
   // Write here the line that prints "Hello world!" and a newline.
   // Hint: if your output is empty in the tests, remember the part about
   // flushing!
-    char* str = "Hello world!";
-    printf("%s\n",  str);
+
+    printf("%s\n",  "Hello world!");
   }
