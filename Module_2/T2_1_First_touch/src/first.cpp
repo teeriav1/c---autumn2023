ed336c9b12e72b445f9b31fa5d054ce993370b17
teeriav1 <85660952+teeriav1@users.noreply.github.com>
2023-09-16 21:46:10 +0300

    Removed a typo of !

--- Module_2/T2_1_First_touch/src/first.cpp
+++ Module_2/T2_1_First_touch/src/first.cpp
@@ -6,6 +6,6 @@ void Hello() {
   // Write here the line that prints "Hello world!" and a newline.
   // Hint: if your output is empty in the tests, remember the part about
   // flushing!
-    char* str = "Hello world!!";
+    char* str = "Hello world!";
     printf("%s\n",  str);
   }
