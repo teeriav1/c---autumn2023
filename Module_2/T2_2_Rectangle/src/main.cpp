bddb4942bb2df27210491dc73ed53a56f901fd04
teeriav1 <85660952+teeriav1@users.noreply.github.com>
2023-09-18 11:44:57 +0300

    Update main.cpp
    Removed additional function, opened include hastag for use

--- Module_2/T2_2_Rectangle/src/main.cpp
+++ Module_2/T2_2_Rectangle/src/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-//#include "rectangle.hpp"
+#include "rectangle.hpp"
 
-void Rectangle() {
-    // Please enter width and height\n
-    float width;
-    float height;
-    printf("%s", "Please enter width and height\n");
-
-    //cin >> width >> height;
-    scanf("%f %f", &width, &height);
-
-    float area = width * height;
-    float Circumference = 2* width + 2* height;
-    printf("Area: %.1f\n", area);
-    printf("Circumference: %.1f\n", Circumference);
-}
 int main(void) {
     Rectangle();
 
